e56fbab6b644df7e7fbb4dcf14313a55218e3045
2777maxim <2777maxim@mail.ru>
2025-01-29 20:11:38 +0300

    Улучшение кодстаил

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto createItem(Long ownerId, ItemDto itemDto); // Создание вещи
+
     ItemDto updateItem(Long id, Long ownerId, ItemDto itemDto); // Обновление вещи
+
     ItemDto getItemById(Long id); // Получение вещи по ID
+
     List<ItemDto> getItemsByOwner(Long ownerId); // Получение всех вещей владельца
+
     List<ItemDto> searchItems(String text); // Поиск вещей по тексту
 }
