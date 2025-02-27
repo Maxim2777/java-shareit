ddebe7cf4a4484780d9736db7ba1691b2d06a986
2777maxim <2777maxim@mail.ru>
2025-02-27 22:23:36 +0300

    schema - убрал удаление таблиц

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 -- Удаляем таблицы в правильном порядке (учитываем внешние ключи)
-DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS bookings CASCADE;
-DROP TABLE IF EXISTS items CASCADE;
-DROP TABLE IF EXISTS requests CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS comments CASCADE;
+-- DROP TABLE IF EXISTS bookings CASCADE;
+-- DROP TABLE IF EXISTS items CASCADE;
+-- DROP TABLE IF EXISTS requests CASCADE;
+-- DROP TABLE IF EXISTS users CASCADE;
 
 -- Создаём таблицу users
 CREATE TABLE IF NOT EXISTS users (
